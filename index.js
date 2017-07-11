9434a07228a5edee8e2175b162a759e8cb520ba3
rsbarbo <rsbarbo@gmail.com>
2017-07-10 20:10:30 -0600

    update router, app, and index

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var express = require('express');
-var route = express.Route();
+var router = express.Router();
+var db = require('./queries');
 
-var db = require('../queries.js');
+function getAllPeople(req, res, next) {
+  db.any('select * from peeps')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved all individuals'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
 
 router.get('/api/people', db.getAllPeople);
-router.get('/api/people/:id', db.getSinglePerson);
-router.post('/api/people', db.createPersonProfile);
-router.put('/api/people/:id', db.updatePerson);
-router.delete('/api/people/:id', db.removePerson);
+// router.get('/api/people/:id', db.getSinglePerson);
+// router.post('/api/people', db.createPersonProfile);
+// router.put('/api/people/:id', db.updatePerson);
+// router.delete('/api/people/:id', db.removePerson);
+
 
 module.exports = router;
--- queries.js
+++ queries.js
@@ -10,10 +10,25 @@ var db = pgp(connectionString);
 
 // add query functions
 
+function getAllPeople(req, res, next) {
+  db.any('select * from peeps')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved all people'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
 module.exports = {
-  getAllPeople: getAllPeople,
-  getSinglePerson: getSinglePerson,
-  createPersonProfile: createPersonProfile,
-  updatePerson: updatePerson,
-  removePerson: removePerson
+  getAllPeople: getAllPeople
+  // getSinglePerson: getSinglePerson,
+  // createPersonProfile: createPersonProfile,
+  // updatePerson: updatePerson,
+  // removePerson: removePerson
 };
